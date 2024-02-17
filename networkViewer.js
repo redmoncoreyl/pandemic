a178b2804bb2615d82c25a34bbd93c9ce3503969
Corey Redmon <redmoncoreyl@gmail.com>
2024-02-17 10:39:47 -0500

    removes randomized node locaions

--- networkViewer.js
+++ networkViewer.js
@@ -1,10 +1,6 @@
 class NetworkViewer {
 	constructor(networkData) {
-		this.networkData = networkData.network;
-		for (let node of Object.values(this.networkData)) {
-			node.location.x = Math.random()*0.8 + 0.1;
-			node.location.y = Math.random()*0.8 + 0.1;
-		}
+		this.networkData = networkData;
 		this.activeNodeId = undefined;
 	}
 
