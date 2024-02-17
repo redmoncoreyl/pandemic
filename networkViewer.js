4dd0a2fa86092d516560e1d69fa6f6ec7f62cabc
Corey Redmon <redmoncoreyl@gmail.com>
2024-02-17 09:57:25 -0500

    implements mousePressed

--- networkViewer.js
+++ networkViewer.js
@@ -5,6 +5,7 @@ class NetworkViewer {
 			node.location.x = Math.random()*0.8 + 0.1;
 			node.location.y = Math.random()*0.8 + 0.1;
 		}
+		this.activeNode = undefined;
 	}
 
 	draw() {
@@ -27,8 +28,17 @@ class NetworkViewer {
 	}
 
 	mousePressed(mouseButton) {
-		if (mouseButton == LEFT) {
-			
+		if (mouseButton != LEFT) return;
+
+		for (let nodeId in this.networkData) {
+			let node = this.networkData[nodeId];
+			let nodeX = node.location.x*width;
+			let nodeY = node.location.x*width;
+			let distance = dist(nodeX, nodeY, mouseX, mouseY);
+			if (distance < 25) {
+				this.activeNode = nodeId;
+				return;
+			}
 		}
 	}
 
