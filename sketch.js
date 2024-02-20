d8270a369736526ff3ca615f2efc9301241d73de
Corey Redmon <redmoncoreyl@gmail.com>
2024-02-19 21:25:43 -0500

    uses percentage to scale network viewer

--- sketch.js
+++ sketch.js
@@ -1,6 +1,6 @@
 window.onresize = () => {
 	resizeCanvas(window.innerWidth, window.innerHeight);
-	networkViewer.resize(10, 10, 1300, 700);
+	networkViewer.resize(10, 10, width*0.8, height*0.9);
 }
 
 document.addEventListener('contextmenu', event => event.preventDefault());
@@ -14,7 +14,7 @@ function preload() {
 
 function setup() {
 	createCanvas(window.innerWidth, window.innerHeight);
-	networkViewer = new NetworkViewer(networkData, 10, 10, 1300, 700);
+	networkViewer = new NetworkViewer(networkData, 10, 10, width*0.8, height*0.9);
 }
 
 function draw() {
